9c1fa737eff4922cef26a48575aa06b047d91c3c
jf10rez <jflorezlondono@hotmail.com>
2022-07-14 16:54:00 -0500

    create navigate to /new

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="dashboard">
-        <h1>¿De cúanto es tu nuevo prestamo?</h1>
+      <h1>¿De cúanto es tu nuevo prestamo?</h1>
 
-        <div className="container-prest">
-            <input className="form-control" placeholder="Monto de prestamo" />
-            <button className="btn btn-primary btn-new-prest"> Nuevo </button>
-        </div>
+      <div className="container-prest">
+        <input className="form-control" placeholder="Monto de prestamo" />
+        <button
+          className="btn btn-success btn-new-prest"
+          onClick={() => navigate("/new")}
+        >
+          Nuevo
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
