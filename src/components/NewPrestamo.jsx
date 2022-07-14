52f5c1cf2a24856753b32a2d7f5a7440651c7062
jf10rez <jflorezlondono@hotmail.com>
2022-07-14 16:55:08 -0500

    /new created

---
+++ src/components/NewPrestamo.jsx
@@ -0,0 +1,13 @@
+import { FormNew } from "./FormNew";
+
+export const NewPrestamo = () => {
+  return (
+    <div className="new">
+      <h1>Completa los siguientes datos</h1>
+
+      <div className="container">
+          <FormNew />
+      </div>
+    </div>
+  );
+};
