cd2a238ff8b0215f9697a9f0999bf0ee7a0538af
jf10rez <jflorezlondono@hotmail.com>
2022-07-18 15:02:04 -0500

    new route

--- src/routers/AnimatedRoutes.jsx
+++ src/routers/AnimatedRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import { Dashboard } from "../components/Dashboard";
 import { NewPrestamo } from "../components/NewPrestamo";
 import { AnimatePresence } from "framer-motion";
+import { ListPrestamos } from "../components/ListPrestamos";
 
 export const AnimatedRoutes = () => {
   const location = useLocation();
@@ -9,6 +10,7 @@ export const AnimatedRoutes = () => {
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
         <Route path="/new" element={<NewPrestamo />} />
+        <Route path="/list" element={<ListPrestamos />} />
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </AnimatePresence>
