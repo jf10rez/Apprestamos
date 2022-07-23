c7441a002c94e1e1b8f354aa8c840a441088e68a
jf10rez <jflorezlondono@hotmail.com>
2022-07-22 23:19:26 -0500

    delete sidebar from prestamos

--- src/pages/Prestamos.jsx
+++ src/pages/Prestamos.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter } from "react-router-dom";
-import { SideBar } from "../components/SideBar";
 
 import "../index.css";
 import { AnimatedRoutes } from "../routers/AnimatedRoutes";
@@ -7,8 +6,6 @@ import { AnimatedRoutes } from "../routers/AnimatedRoutes";
 export const Prestamos = () => {
   return (
     <BrowserRouter>
-      <SideBar />
-
       <div className="max">
         <AnimatedRoutes />
       </div>
