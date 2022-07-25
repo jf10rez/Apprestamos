f3ec1ad9a407e6dcbc36b30f6e2a100efc1d7ad2
jf10rez <jflorezlondono@hotmail.com>
2022-07-25 14:50:30 -0500

    configure store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Prestamos } from './pages/Prestamos'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Prestamos } from "./pages/Prestamos";
 
-import './index.css'
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Prestamos />
+    <Provider store={ store }>
+      <Prestamos />
+    </Provider>
   </React.StrictMode>
-)
+);
