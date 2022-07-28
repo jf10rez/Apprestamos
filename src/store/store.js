5adc2b8b294438b5b390862a2cfc639c6f32eedb
jf10rez <jflorezlondono@hotmail.com>
2022-07-28 11:41:54 -0500

    new slices added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authSlice from './slices/auth/authSlice'
+import prestamoSlice from './slices/prestamos/prestamoSlice'
+import uiSlice from './slices/ui/uiSlice'
 
 export const store = configureStore({
   reducer: {
-    auth: authSlice
+    auth: authSlice,
+    ui: uiSlice,
+    prestamos: prestamoSlice
   },
 })
