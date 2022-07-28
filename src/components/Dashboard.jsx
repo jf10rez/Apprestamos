31f81fe45270237df1614f732c0e18cbffe6f600
jf10rez <jflorezlondono@hotmail.com>
2022-07-28 11:38:39 -0500

    redux and route setted

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,38 @@
 import { useNavigate } from "react-router-dom";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
+import { useDispatch } from "react-redux";
+import { setAmount } from "../store/slices/ui/uiSlice";
+import { useState } from "react";
 
 export const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [amount, setAmountLocal] = useState(null);
+
+  const handleClickNew = () => {
+    navigate("/new");
+    dispatch(setAmount( amount ));
+  };
 
   return (
-    <motion.div className="dashboard"
-      initial={{opacity: 0, x: 100}}
-      animate={{opacity: 1, x: 0}}
-      exit={{opacity: 0, x: -100}}
-      transition={{ease: "easeOut", delay: 0.5}}
+    <motion.div
+      className="dashboard"
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -100 }}
+      transition={{ ease: "easeOut", delay: 0.5 }}
     >
       <h1>¿De cúanto es tu nuevo prestamo?</h1>
 
       <div className="container-prest">
-        <input className="form-control" placeholder="Monto de prestamo" />
+        <input
+          className="form-control"
+          placeholder="Monto de prestamo"
+          onChange={(e) => setAmountLocal(e.target.value)}
+        />
         <button
           className="btn btn-success btn-new-prest"
-          onClick={() => navigate("/new")}
+          onClick={handleClickNew}
         >
           Nuevo
         </button>
