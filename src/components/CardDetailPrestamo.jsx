c99066fcf6784e49dd334461384481b57195ffab
jf10rez <jflorezlondono@hotmail.com>
2022-07-29 16:28:25 -0500

    card of details

---
+++ src/components/CardDetailPrestamo.jsx
@@ -0,0 +1,19 @@
+
+
+export const CardDetailPrestamo = ({ prestamo, setSelectedId }) => {
+  return (
+    <div className="card text-white bg-success mb-3 card-prestamo">
+      <div className="card-header">
+        <span>Header</span>
+        <i className="bi bi-x-circle" onClick={ () => setSelectedId( null ) }></i>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">Success card title</h5>
+        <p className="card-text">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        </p>
+      </div>
+    </div>
+  );
+};
