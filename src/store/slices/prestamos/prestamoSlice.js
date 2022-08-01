a98a5761a5c67ee9bb4ada42902ec34777a1e1ef
jf10rez <jflorezlondono@hotmail.com>
2022-08-01 13:54:54 -0500

    setactive and clearactive prestamo added

--- src/store/slices/prestamos/prestamoSlice.js
+++ src/store/slices/prestamos/prestamoSlice.js
@@ -1,26 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   prestamos: [],
   prestamoSelected: null,
-  dataLoaded: false
-}
+  dataLoaded: false,
+};
 
 export const prestamoSlice = createSlice({
-  name: 'prestamo',
+  name: "prestamo",
   initialState,
   reducers: {
-    addPrestamo: (state, {payload}) => {
-      state.prestamos.push(payload)
+    addPrestamo: (state, { payload }) => {
+      state.prestamos.push(payload);
     },
-    loadPrestamos: ( state, {payload} ) => {
-        state.prestamos = payload
-        state.dataLoaded = true
-    }
+    loadPrestamos: (state, { payload }) => {
+      state.prestamos = payload;
+      state.dataLoaded = true;
+    },
+    setActivePrestamo: (state, { payload }) => {
+      state.prestamoSelected = payload;
+    },
+    clearActivePrestamo: (state) => {
+      state.prestamoSelected = null;
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addPrestamo, loadPrestamos } = prestamoSlice.actions
+export const {
+  addPrestamo,
+  loadPrestamos,
+  setActivePrestamo,
+  clearActivePrestamo,
+} = prestamoSlice.actions;
 
-export default prestamoSlice.reducer
+export default prestamoSlice.reducer;
