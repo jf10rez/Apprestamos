40ab215cd32e1d964dc7ac12743a40067b44455c
JF <jhonatan.florez@integ.ro>
2022-08-05 23:12:41 -0500

    pin component works!

--- src/components/PinComponent.jsx
+++ src/components/PinComponent.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import PinInput from "react-pin-input";
+import { useDispatch } from "react-redux";
+import { startModifyPrestamo } from "../store/slices/prestamos/prestamoThunks";
 
-export const PinComponent = ({ isModalOpen }) => {
+export const PinComponent = ({ isModalOpen, setIsModalOpen, data }) => {
+
+  const [pin, setPin] = useState("")
+  const dispatch = useDispatch()
+
+  const handleCloseModal = () => {
+      setIsModalOpen( false )
+  }
+
+  const handleConfirm = () => {
+    if( !pin ) return
+
+    data.pin = pin
+
+    dispatch( startModifyPrestamo( data ) )
+    setIsModalOpen(false)
+
+  }
 
   return (
     <Modal isOpen={isModalOpen} ariaHideApp={false} id="modal-pin">
@@ -10,22 +30,21 @@ export const PinComponent = ({ isModalOpen }) => {
         length={4}
         initialValue=""
         secret
-        onChange={(value, index) => {}}
+        onChange={(value, index) => setPin( value )}
         type="numeric"
         inputMode="number"
         style={{ padding: "10px" }}
         inputStyle={{ borderColor: "white" }}
         inputFocusStyle={{ borderColor: "lightgreen" }}
-        onComplete={(value, index) => {}}
         autoSelect={true}
         regexCriteria={/^[ A-Za-z0-9_@./#&+-]*$/}
       />
       <div className="row p-3">
         <div className="col-md-6 col-sm-6">
-          <button className="btn btn-danger">Cancelar</button>
+          <button className="btn btn-danger" onClick={handleCloseModal}>Cancelar</button>
         </div>
         <div className="col-md-6 col-sm-6">
-          <button className="btn btn-success">Confirmar</button>
+          <button className="btn btn-success" onClick={ handleConfirm }>Confirmar</button>
         </div>
       </div>
     </Modal>
